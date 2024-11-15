0630d8957f5244785e73a30932aa843375c93396
Antanina <tonia@Antanina-MacBook-Pro.local>
2024-11-15 22:37:03 +0200

    added database envs

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -5,7 +5,13 @@ let sql;
 if (Deno.env.get("DATABASE_URL")) {
   sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
-  sql = postgres({});
+  sql = postgres({
+    user: Deno.env.get("POSTGRES_USER"),
+    password: Deno.env.get("POSTGRES_PASSWORD"),
+    database: Deno.env.get("POSTGRES_DB"),
+    hostname: Deno.env.get("POSTGRES_HOST"),
+    port: Number(Deno.env.get("POSTGRES_PORT")),
+  });
 }
 
 export { sql };
